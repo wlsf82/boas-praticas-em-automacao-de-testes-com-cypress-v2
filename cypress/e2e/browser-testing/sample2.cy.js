dac873ba603441b4e45b757a2d4d0c928ab723e9
João Canabarro Campos <jcampos@Joaos-MBP.lan>
2024-01-09 16:01:07 +0100

    QA - New codes and Tests

--- cypress/e2e/browser-testing/sample2.cy.js
+++ cypress/e2e/browser-testing/sample2.cy.js
@@ -5,10 +5,7 @@ describe('Browser testing bad practice - anchor with target _blank', () => {
 
   it('directs the user to the privacy page after removing the target and clicking the link', () => {
     cy.contains('a', 'Política de Privacidade')
-      .invoke('removeAttr', 'target')
-      .click()
-
-    cy.url()
-      .should('be.equal', 'https://cac-tat.s3.eu-central-1.amazonaws.com/privacy.html')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
   })
 })
--- cypress/e2e/duplication/sample1.cy.js
+++ cypress/e2e/duplication/sample1.cy.js
@@ -1,19 +1,19 @@
 describe('Code duplication bad practice - repetitive steps', () => {
-  it('searches by typing and hitting enter', () => {
+  beforeEach(() => {
     cy.intercept(
       'GET',
       '**/search**'
     ).as('getStories')
-
     cy.visit('https://hackernews-seven.vercel.app')
     cy.wait('@getStories')
+  })
+  it('searches by typing and hitting enter', () => {
 
     cy.get('input[type="text"]')
       .should('be.visible')
       .and('have.value', 'redux')
       .clear()
       .type('frontend testing{enter}')
-
     cy.wait('@getStories')
 
     cy.get('.table-row')
@@ -21,13 +21,6 @@ describe('Code duplication bad practice - repetitive steps', () => {
   })
 
   it('searches by typing and pressing the search button', () => {
-    cy.intercept(
-      'GET',
-      '**/search**'
-    ).as('getStories')
-
-    cy.visit('https://hackernews-seven.vercel.app')
-    cy.wait('@getStories')
 
     cy.get('input[type="text"]')
       .should('be.visible')
